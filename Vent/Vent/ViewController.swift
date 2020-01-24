6fbfb2833cc01b962be21ef844395ad132c14cc1
Evan <evy0311@gmail.com>
2020-01-23 22:29:48 -0500

    Added UI elements to storyboard

--- Vent/Vent/ViewController.swift
+++ Vent/Vent/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rantBox: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        rantBox.text = ""
+    }
+    
 }
 
