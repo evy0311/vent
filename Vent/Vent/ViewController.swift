ec7e35cdad10ab7e3a7c5fb3cb1cd9dcba648d84
Evan <evy0311@gmail.com>
2020-01-23 21:54:42 -0500

    Updated to use Storyboards

---
+++ Vent/Vent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vent
+//
+//  Created by Evan Horsley on 1/23/20.
+//  Copyright © 2020 Evan Horsley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
